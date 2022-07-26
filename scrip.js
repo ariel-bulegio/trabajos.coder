ccff84fdb606cdc43dcff77f6e304571b80db0ad
Ariel Bulegio <arielmaita90@gmail.com>
2022-07-25 23:46:44 -0300

    desafio js

---
+++ scrip.js
@@ -0,0 +1,17 @@
+let ingresar = prompt ("Escriba algo")
+
+while(ingresar != "ESC"){ 
+    switch (ingresar){
+        case "Buenos Dias":
+            alert ("Hola Buenos Dias");
+            break ;
+            case "Como Estas":
+                alert ("Hola Como Estas");
+                break;
+                default:
+                    alert ("Quien sos")
+                    break;
+
+    }
+ingresar = prompt ("Escriba algo")
+}
